8f0fa6ebb58186d00ac4e3dd936f1c102f0bccfe
dsushanta <dasgupta.sushanta@gmail.com>
2020-10-20 21:31:59 +0530

    added p tag in display.js

--- src/components/practice/Display.js
+++ src/components/practice/Display.js
@@ -4,6 +4,7 @@ export default class Display extends Component {
     render() {
         return (
             <div>
+            	<p> My name is Sushant</p> 
                 <h1> My name is Urmisha</h1>
             </div>
         )
