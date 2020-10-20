8dc70f235b8a3f95c95df1c832c26cec5658ce37
UrmishaDas <ud.rocks13@gmail.com>
2020-10-20 21:26:04 +0530

    Added folder display to components

---
+++ src/components/practice/Display.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Display extends Component {
+    render() {
+        return (
+            <div>
+                <h1> My name is Urmisha</h1>
+            </div>
+        )
+    }
+}
