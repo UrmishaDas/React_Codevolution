d97267d944b08b2d29adb59319fb3dfe39ce5493
dsushanta <dasgupta.sushanta@gmail.com>
2020-10-20 22:00:23 +0530

    Added new feature called Welcome

--- src/components/practice/Welcome.js
+++ src/components/practice/Welcome.js
@@ -4,9 +4,7 @@ export default class Display extends Component {
     render() {
         return (
             <div>
-                <h1> My name is Urmisha Das</h1>
-            	<h2>And I am Timon</h2>
-            	<p> My name is Sushant</p>
+                <h1> Welcome Timon</h1>
             </div>
         )
     }
